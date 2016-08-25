63d4b114d37308a2bdcc8ad328295e6aa85934b7
bindkeeper <bindkeeper@gmail.com>
2016-08-25 19:36:26 +0300

    changed the urls

--- travelSite/urls.py
+++ travelSite/urls.py
@@ -21,4 +21,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^organizer/', include('organizer.urls')),
+	url(r'^', include('organizer.urls')),
 ]
