1b8c061f940f1549daa101cac7e4c0a34a24f6a1
bindkeeper <bindkeeper@gmail.com>
2016-09-08 16:04:41 +0300

    removed alert after share

--- organizer/static/organizer/main.js
+++ organizer/static/organizer/main.js
@@ -6,7 +6,7 @@ $(".btn-share").click(function(){
 	
 	var url = $(this).attr('href');
 	var self = $(this);
-	alert(url);
+	
     $.getJSON(url, function(result){
         $.each(result, function(result) {
 			if (result.success) {
